057096c87079d86e8aee4fed915963c5c4b31641
Hemil Chanlawala <hdchanlawala.com>
2026-06-24 22:30:31 +0530

    conflit solvee1

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-/* eslint-disable react-refresh/only-export-components */
->>>>>>> 07a3d598e0971ee3e2dd567faee5f43c136d6b7c
 import { createContext } from 'react'
 import useAuth from '../hooks/useAuth'
 
@@ -9,7 +5,6 @@ export const AuthContext = createContext(null)
 
 export const AuthProvider = ({ children }) => {
   const auth = useAuth()
-<<<<<<< HEAD
 
   return (
     <AuthContext.Provider value={auth}>
@@ -19,9 +14,3 @@ export const AuthProvider = ({ children }) => {
 }
 
 export default AuthContext
-=======
-  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
-}
-
-export default AuthContext
->>>>>>> 07a3d598e0971ee3e2dd567faee5f43c136d6b7c
